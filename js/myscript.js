5f1ec0dc8d54f995636fc9d81f9118939d3e2683
Minh Duc Ha <ducha180102@gmail.com>
2022-12-21 07:49:02 +0100

    connect with javascripts file

--- js/myscript.js
+++ js/myscript.js
@@ -6,7 +6,38 @@ function caculateAge(yearBorn) {
 	return curentYear-yearBorn;
 }
 var name1=prompt("Enter your name");
-var yearBorn=prompt("Enter the year you born");
+	var yearBorn=prompt("Enter the year you born");
 var age=caculateAge(yearBorn);
 
 console.log(name1 + " ist " + age + " years old");
+
+var message="";
+if (age<18){
+	message+="You are"
+	var i=0;
+	while(i<5){
+		message+=" very";
+		i++;
+	}
+	message += " young";
+	if ((age>5) && (age<10)){
+		message="You are a kid";
+	}
+
+}else if(age<50){
+	message="You are quite young";
+}else{
+	message="You are over 50 years old";
+}
+
+console.log(message);
+
+
+
+var isClosed= confirm("abc");
+
+if (isClosed===true){
+	console.log("Bye Bye");
+}else{
+	console.log("Thanks");
+}
