736097f543e2851cb327938f210e82a157b963f6
Ha <vd754130@win.rz.rwth-aachen.de>
2022-12-22 10:32:30 +0100

    add function for scores table

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,24 @@
 function init(){
-var sum_html=document.getElementById("btn");
-sum_html.onclick = averageCaculation;
+	var sum_html=document.getElementById("btn");
+	sum_html.onclick = averageCaculation;
+	var scores=document.getElementsByTagName("td");
+	var i=0;
+	while (i<scores.length){
+		scores[i].onclick = changeValue;
+		i++
+	}
+}
+
+function changeValue(){
+	while (true){
+		var newValue= prompt("Which Scores is it?");
+		if ((typeof Number(newValue)==="number") && (Number(newValue)<=10&&Number(newValue)>=0)){
+			break
+		}else{
+			alert("Input Value isn't number.Please try again");
+		}
+	}
+	this.textContent = newValue;
 }
 function averageCaculation(){
 	var scores=document.getElementsByTagName("td");
@@ -11,6 +29,7 @@ function averageCaculation(){
 	}
 	average=average/scores.length;
 	document.getElementById("res").innerText=average;
+	this.textContent= "Caculate again";
 }
 
 window.onload = init ;
