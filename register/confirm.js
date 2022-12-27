f8f1abeed45c129f779a19b3add990e7c48b28fd
Minh Duc Ha <tomreal180@gmail.com>
2022-12-27 02:11:49 +0100

    checkbox and dropbox in reǵister.js edit

--- register/confirm.js
+++ register/confirm.js
@@ -1,17 +1,36 @@
 function loadData(){
+    //call tags which need to be edited
     var name= document.getElementById("confirm-name");
     var email= document.getElementById("confirm-mail");
     var phonenumber= document.getElementById("confirm-phonenumber");
     var age= document.getElementById("confirm-age");
     var degree= document.getElementById("confirm-degree");
-    
-    
+    var requirement=document.getElementById("confirm-requirement");
+    var major= document.getElementById("confirm-major");
+
+    //change a text content
     lastname=sessionStorage.getItem("lastname");
     name.textContent = sessionStorage.firstname+" "+lastname;
     email.textContent = sessionStorage.email;
     phonenumber.textContent = sessionStorage.phonenumber;
     age.textContent = sessionStorage.age;
-    degree.textContent= sessionStorage.degree;}
+    degree.textContent= sessionStorage.degree;
+    major.textContent= sessionStorage.major;
+    //change radio requirement
+    var text="";
+    var allrequire= sessionStorage.getItem("allrequirement").split(",");
+    var i=0;
+    while(i<allrequire.length){
+        if (sessionStorage.getItem(allrequire[i]) == "true"){
+            text+=allrequire[i]+", ";
+        }
+        i++;
+    }
+    text= text.substring(0,text.length-2);
+    requirement.textContent = text;
+    console.log("abc");
+}
+
 
 function returnregis(){
     window.location= "register.html";
@@ -22,6 +41,7 @@ function init(){
     loadData();
     var cancel=document.getElementById("cancelButton");
     cancel.onclick = returnregis;
+
 }
 
 window.onload =init;
--- register/register.js
+++ register/register.js
@@ -1,4 +1,5 @@
 function saveData(){
+    //save date in information box
     var firstname= document.getElementById("firstname").value;
     var lastname= document.getElementById("lastname").value;
     var email= document.getElementById("email").value;
@@ -10,50 +11,63 @@ function saveData(){
     sessionStorage.email= email;
     sessionStorage.phonenumber= phonenumber;
     sessionStorage.age= age;
+    //save data in radio degree
     var degreeArray = document.getElementById("radioDegree").getElementsByTagName("input");
     for(var i=0; i< degreeArray.length; i++){
         if( degreeArray[i].checked == true){
             sessionStorage.degree= degreeArray[i].value;
         }
     }
-
+    //save data in radio Requirement 
     var requireArray=document.getElementById("radioRequire").getElementsByTagName("input");
     var i=0;
+    var allrequirement=[];
     while(i<requireArray.length){
+        console.log("123");
         if (requireArray[i].checked ==true){
-            sessionStorage.setItem(requireArray[i].name,requireArray[i].value);
+            sessionStorage.setItem(requireArray[i].name,true);
         }else{
-            if (sessionStorage.getItem(requireArray[i].name) != null){
-                sessionStorage.setItem(requireArray[i].name,null);
+            if (sessionStorage.getItem(requireArray[i].name) != " true"){
+                sessionStorage.setItem(requireArray[i].name,false);
             }
         }
+        allrequirement.push(requireArray[i].name);
         i+=1;
     }
-
+    sessionStorage.allrequirement = allrequirement;
+    //save data in major dropbox
+    var majorselect=document.getElementById("major");
+    sessionStorage.setItem("major",majorselect.value);
 }
 
 function prefill(){
     if (sessionStorage.firstname != null){
+        //prefill information box 
         document.getElementById("firstname").value= sessionStorage.firstname;
         document.getElementById("lastname").value= sessionStorage.lastname;
         document.getElementById("email").value= sessionStorage.email;
         document.getElementById("phonenumber").value= sessionStorage.phonenumber;
         document.getElementById("age").value= sessionStorage.age;
-
+        //prefill radio degree
         var degreeArray= document.getElementById("radioDegree").getElementsByTagName("input");
         for(var i=0; i< degreeArray.length; i++){
             if( degreeArray[i].value == sessionStorage.degree){
                 degreeArray[i].checked=true;
             }
         }
-
+        //prefill radio requirement 
         var requireArray=document.getElementById("radioRequire").getElementsByTagName("input");
         var i=0;
         while(i<requireArray.length){
-            if (requireArray[i].value == sessionStorage.getItem(requireArray[i].name)){
+            if (sessionStorage.getItem(requireArray[i].name)=="true"){
                 requireArray[i].checked = true;
             }
             i+=1;
+        }
+        //prefill dropbox major
+        var majorselect=document.getElementById("major");
+        if(sessionStorage.major!= null){
+            majorselect.value = sessionStorage.major;
         }
 
     }
